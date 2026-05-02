ea5d94f88529969e90b4ee0a9a2cff4cf82184cd
parfeniukOlena <varenyko2015@gmail.com.>
2026-05-02 22:01:26 +0300

    done

---
+++ script.js
@@ -0,0 +1,96 @@
+// 1 модалка
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
+
+// 2 модалка
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open2]"),
+        closeModalBtn: document.querySelector("[data-modal-close2]"),
+        modal: document.querySelector("[data-modal2]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
+
+
+// 3 модалка
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open3]"),
+        closeModalBtn: document.querySelector("[data-modal-close3]"),
+        modal: document.querySelector("[data-modal3]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
+
+// 4 модалка
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open4]"),
+        closeModalBtn: document.querySelector("[data-modal-close4]"),
+        modal: document.querySelector("[data-modal4]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
+
+// 5 модалка
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open5]"),
+        closeModalBtn: document.querySelector("[data-modal-close5]"),
+        modal: document.querySelector("[data-modal5]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
+
+// 6 модалка
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open6]"),
+        closeModalBtn: document.querySelector("[data-modal-close6]"),
+        modal: document.querySelector("[data-modal6]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
